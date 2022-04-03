b4b195cbf5c8e8215daf2f916119536d5a2c2407
Ridmi <86437101+IT20242022@users.noreply.github.com>
2022-04-03 13:31:40 +0530

    Update server.js
    User Routes configured in server

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,9 @@ app.use("/driver", driverRouter);
 const bookingRouter = require("./routes/bookings");
 app.use("/booking", bookingRouter);
 
+const userRoutes = require("./routes/UserRoutes");
+app.use('/user', userRoutes);
+
 app.listen(PORT, ()=>{
     console.log(`Server is up and running on port number: ${PORT}`)
 })
