0f425a73151a2a82c2dc0f1c8009de03899bf7e2
Ridmi <86437101+IT20242022@users.noreply.github.com>
2022-05-08 21:25:33 +0530

    Create UserProfile.js

---
+++ frontend/RIDEOUT-frontend/Project-CRUD-React-App-for-an-Online-Car-Rental-Company-main/src/Components/User/UserProfile/UserProfile.js
@@ -0,0 +1,237 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { MainContext } from '../../../Contexts/MainContext'
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    Col,
+    Form,
+    Spinner,
+    ToggleButton,
+} from "react-bootstrap";
+import Swal from "sweetalert2";
+import { deleteUserAccountService, updateUserAccountService } from '../../../Services/UserServices'
+import { UpdateUserAccountValidations } from '../../../Validations/UserValidations'
+import { SweetAlert } from '../../../Services/SweetAlert'
+
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: "",
+            name: "",
+            email: "",
+            mobile: "",
+            gender: "",
+            nic: "",
+            loading: true,
+            updating: false,
+            deleting: false,
+        };
+    }
+
+    updateProfileDetails = () => {
+        let user = JSON.parse(localStorage.getItem("user"));
+        if (!user) {
+            this.props.history.push("/SignIn");
+        } else {
+            this.setState({
+                loading: false,
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                mobile: user.mobile,
+                gender: user.gender,
+                nic: user.nic,
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.updateProfileDetails()
+    }
+
+    static contextType = MainContext;
+
+    // Function to handle input change
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // Function to handle gender change
+    handleGender = (gender) => {
+        this.setState({
+            gender,
+        });
+    };
+
+    // Delete account function
+    deleteMyAccount = () => {
+        this.setState({
+            loading: true,
+            deleting: true
+        })
+        Swal.fire({
+            title: 'Confirmation',
+            text: "Are you want to delete your account.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, Delete'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteUserAccountService(this.state.id).then(res => {
+                    if (res.status) {
+                        this.context.setIsAuthenticated(false);
+                        this.props.history.push("/SignIn");
+                    }
+                })
+            }
+        })
+
+        this.setState({
+            loading: false,
+            deleting: false
+        })
+    }
+
+    updateUser = async (e) => {
+        e.preventDefault();
+
+        let updatedUser = {
+            name: this.state.name,
+            mobile: this.state.mobile,
+            gender: this.state.gender,
+            nic: this.state.nic,
+        }
+
+        const result = UpdateUserAccountValidations(updatedUser);
+        if (result.status) {
+            await updateUserAccountService(this.state.id, updatedUser).then(res => {
+                this.setState({
+                    loading: true,
+                    updating: true
+                });
+                if (res.status) {
+                    this.setState({
+                        loading: false,
+                        updating: false
+                    });
+                    this.updateProfileDetails();
+                }
+            })
+        } else {
+            SweetAlert("error", "Ooopz!", result.error)
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Card>
+                    <Card.Body>
+                        <Card.Title className="text-center fs-2">User Profile</Card.Title>
+                        <Form onSubmit={this.updateUser}>
+                            <Form.Group className="mb-3">
+                                <Form.Label className="text-start">User Email</Form.Label>
+                                <Form.Control
+                                    disabled
+                                    value={this.state.email}
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label className="text-start">User Name</Form.Label>
+                                <Form.Control
+                                    disabled={this.state.loading}
+                                    name="name"
+                                    value={this.state.name}
+                                    onChange={(e) => this.handleInputChange(e)}
+                                    placeholder="Enter user name"
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label className="text-start">Mobile Number</Form.Label>
+                                <Form.Control
+                                    disabled={this.state.loading}
+                                    name="mobile"
+                                    value={this.state.mobile}
+                                    onChange={(e) => this.handleInputChange(e)}
+                                    placeholder="Enter mobile number"
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label className="text-start">NIC</Form.Label>
+                                <Form.Control
+                                    disabled={this.state.loading}
+                                    name="nic"
+                                    value={this.state.nic}
+                                    onChange={(e) => this.handleInputChange(e)}
+                                    placeholder="Enter NIC"
+                                />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Label className="text-start d-block">Gender</Form.Label>
+                                <ButtonGroup>
+                                    <ToggleButton
+                                        disabled={this.state.loading}
+                                        key={"male"}
+                                        id={"male"}
+                                        type="radio"
+                                        name="male"
+                                        checked={this.state.gender === "male"}
+                                        onChange={(e) => this.handleGender("male")}
+                                        variant={'outline-primary'}
+                                    >
+                                        Male
+                                    </ToggleButton>
+                                    <ToggleButton
+                                        disabled={this.state.loading}
+                                        key={"female"}
+                                        id={"female"}
+                                        type="radio"
+                                        name="female"
+                                        checked={this.state.gender === "female"}
+                                        onChange={() => this.handleGender("female")}
+                                        variant={'outline-primary'}
+                                    >
+                                        Female
+                                    </ToggleButton>
+                                </ButtonGroup>
+                            </Form.Group>
+                            <Col className="text-center">
+                                <Button variant="success" type="submit" className="me-1" disabled={this.state.loading}>
+                                    <Spinner
+                                        as="span"
+                                        animation="border"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                        className={this.state.loading && this.state.updating ? "" : "visually-hidden"}
+                                    />
+                                    <span className="ms-2">{this.state.loading && this.state.updating ? "Please Wait..." : "Update Profile"}</span>
+                                </Button>
+                                <Button variant="danger" className="me-1" disabled={this.state.loading} onClick={() => this.deleteMyAccount()}>
+                                    <Spinner
+                                        as="span"
+                                        animation="border"
+                                        size="sm"
+                                        role="status"
+                                        aria-hidden="true"
+                                        className={this.state.loading && this.state.deleting ? "" : "visually-hidden"}
+                                    />
+                                    <span className="ms-2">{this.state.loading && this.state.deleting ? "Please Wait..." : "Delete Profile"}</span>
+                                </Button>
+                            </Col>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </React.Fragment>
+        );
+    }
+}
+
+export default withRouter(UserProfile);
